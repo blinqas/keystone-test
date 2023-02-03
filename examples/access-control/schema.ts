0695a50a20087657c0a7392e93052541a583aaee
Daniel Cousens <dcousens@users.noreply.github.com>
2023-02-03 14:43:05 +1100

    add access-control example

---
+++ examples/access-control/schema.ts
@@ -0,0 +1,54 @@
+import { list } from '@keystone-6/core';
+import { allowAll } from '@keystone-6/core/access';
+import { text, checkbox } from '@keystone-6/core/fields';
+import { Lists } from '.keystone/types';
+
+function isAdmin({ session }: { session: any }) {
+  // not a good example, but helpful for debugging
+  return true;
+}
+
+export const lists: Lists = {
+  // an example of field access control
+  User: list({
+    access: allowAll,
+    fields: {
+      // this field should be publicly readable
+      displayName: text({
+        access: {
+          read: () => true,
+        },
+      }),
+
+      // this field should only be readable by administrators
+      name: text({
+        access: {
+          read: isAdmin,
+          create: isAdmin,
+          update: isAdmin,
+        },
+        isFilterable: false,
+        isOrderable: false,
+      }),
+
+      email: text({
+        access: {
+          read: isAdmin,
+        },
+        isFilterable: false,
+        isOrderable: false,
+      }),
+
+      isAdmin: checkbox({
+        access: {
+          read: isAdmin,
+        },
+        isFilterable: false,
+        isOrderable: false,
+      }),
+    },
+    graphql: {
+      omit: ['create', 'delete'],
+    },
+  }),
+};
