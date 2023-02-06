af077663e1833977bed5b4cd50f41f8986935e96
Daniel Cousens <dcousens@users.noreply.github.com>
2023-02-06 17:31:10 +1100

    merge auth/ and access-control/ examples

--- examples/auth/schema.ts
+++ examples/auth/schema.ts
@@ -1,17 +1,43 @@
 import { list } from '@keystone-6/core';
-import { allOperations, allowAll } from '@keystone-6/core/access';
+import { allowAll } from '@keystone-6/core/access';
 import { text, checkbox, password } from '@keystone-6/core/fields';
 import type { Lists } from '.keystone/types';
 
-function isAdminOrSameUser({ session, item }: { session: any; item: Lists.User.Item }) {
+type Session = {
+  itemId: string;
+  data?: {
+    isAdmin: boolean;
+  };
+}
+
+function hasSession({ session }: { session: Session | undefined }) {
+  return Boolean(session)
+}
+
+function isAdminOrSameUser({ session, item }: { session: Session | undefined; item: Lists.User.Item }) {
   // you need to have a session to do this
   if (!session) return false;
 
   // admins can do anything
-  if (session.data.isAdmin) return true;
+  if (session.data?.isAdmin) return true;
 
   // the authenticated user needs to be equal to the user we are updating
   return session.itemId === item.id;
+}
+
+function isAdminOrSameUserFilter({ session }: { session: Session | undefined; }) {
+  // you need to have a session to do this
+  if (!session) return false;
+
+  // admins can see everything
+  if (session.data?.isAdmin) return {};
+
+  // the authenticated user can only see themselves
+  return {
+    id: {
+      equals: session.itemId
+    }
+  }
 }
 
 function isAdmin({ session }: { session: any }) {
@@ -29,10 +55,22 @@ export const lists: Lists = {
   User: list({
     access: {
       operation: {
-        ...allOperations(allowAll),
+        create: allowAll,
+        query: allowAll,
+
+        // only allow users to update _anything_, but what they can update is limited by
+        //   the access.filter.* and access.item.* access controls
+        update: hasSession,
+
         // only allow admins to delete users
         delete: ({ session }) => session?.data.isAdmin,
       },
+      filter: {
+        update: isAdminOrSameUserFilter
+      },
+      item: {
+        update: isAdminOrSameUser
+      }
     },
     ui: {
       // since you can't delete users unless you're an admin, we hide the UI for it
@@ -48,11 +86,25 @@ export const lists: Lists = {
 
       // the user's email address, used as the identity field for authentication
       //   we use isIndexed to enforce this email is unique
-      email: text({ isIndexed: 'unique', validation: { isRequired: true } }),
+      email: text({
+        // this field should only be readable by the respective user, or administrators
+        access: {
+          read: isAdminOrSameUser,
+          update: isAdminOrSameUser
+        },
+
+        isFilterable: false,
+        isOrderable: false,
+        isIndexed: 'unique',
+        validation: {
+          isRequired: true
+        }
+      }),
 
       // the user's password, used as the secret field for authentication
       password: password({
         access: {
+          read: isAdminOrSameUser,
           update: isAdminOrSameUser,
         },
         ui: {
